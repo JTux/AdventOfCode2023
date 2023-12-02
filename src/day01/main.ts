546530ab64fd13f9ca4e6e815e9a7c0503aae86d
Joshua Tucker <the1jtux@gmail.com>
2023-12-02 01:50:11 -0500

    Day 1

--- src/day01/main.ts
+++ src/day01/main.ts
@@ -20,6 +20,99 @@
 
 // Consider your entire calibration document. What is the sum of all of the calibration values?
 
-import { getFile } from "@utils/getFile";
+// --- Part Two ---
+// Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
 
-console.log(getFile("day01"));
+// Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+// two1nine
+// eightwothree
+// abcone2threexyz
+// xtwone3four
+// 4nineeightseven2
+// zoneight234
+// 7pqrstsixteen
+// In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+// What is the sum of all of the calibration values?
+
+import { EOL } from "os";
+import { getFile } from "../../utils/getFile";
+
+const file = getFile("day01");
+
+const lineNumbers = file.split(EOL).map(line => {
+  const nums = line.replace(/\D/g, "");
+
+  if (!nums) return NaN;
+
+  const first = nums[0];
+  const last = nums[nums.length - 1];
+
+  const lineNumber = first + "" + last;
+
+  return Number(lineNumber);
+});
+
+const sum = lineNumbers.reduce((sum, current) => sum + current, 0);
+console.log("Part 1: ", sum);
+
+const digits = {
+  "one": "1",
+  "two": "2",
+  "three": "3",
+  "four": "4",
+  "five": "5",
+  "six": "6",
+  "seven": "7",
+  "eight": "8",
+  "nine": "9",
+  "1": "1",
+  "2": "2",
+  "3": "3",
+  "4": "4",
+  "5": "5",
+  "6": "6",
+  "7": "7",
+  "8": "8",
+  "9": "9"
+};
+const validDigits = [
+  "1", "2", "3", "4", "5", "6", "7", "8", "9",
+  "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+] as const;
+
+const lineNumbers2 = file
+  .split(EOL)
+  .map(line => {
+    let allMatches = validDigits.flatMap(d => {
+      let searchIndex = 0;
+      let i: number;
+      let indices = [];
+
+      while ((i = line.indexOf(d, searchIndex)) > -1) {
+        indices.push({ index: i, value: d });
+        searchIndex = i + d.length;
+      }
+
+      return indices;
+    });
+
+    const validMatches = allMatches.filter((num) => num.index != -1);
+    const orderedMatches = validMatches.sort((a, b) => a.index - b.index);
+
+    const numbers = orderedMatches.map(i => {
+      if (isNaN(Number(i.value)))
+        return digits[i.value]
+
+      return i.value
+    });
+
+    const first = numbers[0];
+    const last = numbers[numbers.length - 1];
+    const lineNumber = first + "" + last;
+    return Number(lineNumber);
+  });
+
+const sum2 = lineNumbers2.reduce((sum, current) => sum + current, 0);
+console.log("Part 2: ", sum2);
